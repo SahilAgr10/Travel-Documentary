31c1385b1ec2a5f1fdbcad7771773fa6da6cbaa0
Sahil Agrawal <agrawalsahil4994@gmail.com>
2023-02-21 22:23:21 +0530

    UI Build up

--- app/src/main/java/com/example/traveldocumentary/MainActivity.java
+++ app/src/main/java/com/example/traveldocumentary/MainActivity.java
@@ -2,13 +2,35 @@ package com.example.traveldocumentary;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // on below line we are
+                // creating a new intent
+                Intent i = new Intent(MainActivity.this, HomeScreen.class);
+
+                // on below line we are
+                // starting a new activity.
+                startActivity(i);
+
+                // on the below line we are finishing
+                // our current activity.
+                finish();
+            }
+        }, 2000);
     }
 }
