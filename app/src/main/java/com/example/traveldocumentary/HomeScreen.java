68eb325c2067c27bcdeae4999e53789c17c75cc7
Sahil Agrawal <agrawalsahil4994@gmail.com>
2023-02-26 20:17:20 +0530

    UI Build up

--- app/src/main/java/com/example/traveldocumentary/HomeScreen.java
+++ app/src/main/java/com/example/traveldocumentary/HomeScreen.java
@@ -3,6 +3,10 @@ package com.example.traveldocumentary;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class HomeScreen extends AppCompatActivity {
 
@@ -11,8 +15,20 @@ public class HomeScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
 
-        String[] tabs = {"Upcoming Travel ","Past Travels"," Recommended Travels "};
+        String[] tabs = {" Upcoming Travel "," Past Travels"," Recommended Travels "};
+        LinearLayout rootview = (LinearLayout) findViewById(R.id.rootview);
 
+//        for(int i =0;i<tabs.length;i++){
+//            TextView word = new TextView(this);
+//            word.setText(tabs[i]);
+//            word.setPadding(40,40,40,40);
+//            word.setTextSize(20);
+//            rootview.addView(word);
+//        }
+
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, tabs);
+        ListView listView = (ListView) findViewById(R.id.list);
+        listView.setAdapter(itemsAdapter);
 
 
     }
