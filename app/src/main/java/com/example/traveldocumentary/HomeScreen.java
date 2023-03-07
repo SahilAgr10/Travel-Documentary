0afc59eceac01e950ba38e940de2166f99cc1f0b
Sahil Agrawal <agrawalsahil4994@gmail.com>
2023-03-07 23:38:35 +0530

    SQLite Database Connections

--- app/src/main/java/com/example/traveldocumentary/HomeScreen.java
+++ app/src/main/java/com/example/traveldocumentary/HomeScreen.java
@@ -19,7 +19,7 @@ public class HomeScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
-        getWindow().setStatusBarColor(ContextCompat.getColor(HomeScreen.this,R.color.black));
+        getWindow().setStatusBarColor(ContextCompat.getColor(HomeScreen.this,R.color.creamWhiteFaint));
 
 //        TextView textView = new TextView(this);
 //        textView.setText("SAHIL");
