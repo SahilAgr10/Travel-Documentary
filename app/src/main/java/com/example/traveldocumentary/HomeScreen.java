4e409316cab37b025b32be302f7cecb1a39a0a6d
Sahil Agrawal <agrawalsahil4994@gmail.com>
2023-03-10 18:48:04 +0530

    SQLite Database Connections

--- app/src/main/java/com/example/traveldocumentary/HomeScreen.java
+++ app/src/main/java/com/example/traveldocumentary/HomeScreen.java
@@ -3,6 +3,8 @@ package com.example.traveldocumentary;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -10,6 +12,9 @@ import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
+
+import com.example.traveldocumentary.SQLiteDatabaseFiles.DatabaseHelper;
+import com.example.traveldocumentary.SQLiteDatabaseFiles.DestinationContracts;
 
 import java.util.ArrayList;
 
@@ -39,5 +44,30 @@ public class HomeScreen extends AppCompatActivity {
         TabsInfoAdapter itemsAdapter = new TabsInfoAdapter(this,list);
         ListView listView = (ListView) findViewById(R.id.list);
         listView.setAdapter(itemsAdapter);
+
+        displayDatabaseInfo();
     }
+    private void displayDatabaseInfo() {
+        // To access our database, we instantiate our subclass of SQLiteOpenHelper
+        // and pass the context, which is the current activity.
+        DatabaseHelper mDbHelper = new DatabaseHelper(this);
+
+        // Create and/or open a database to read from it
+        SQLiteDatabase db = mDbHelper.getReadableDatabase();
+
+        // Perform this raw SQL query "SELECT * FROM pets"
+        // to get a Cursor that contains all rows from the pets table.
+        Cursor cursor = db.rawQuery("SELECT * FROM " + DestinationContracts.DesinationEntry.TABLE_NAME, null);
+        try {
+            // Display the number of rows in the Cursor (which reflects the number of rows in the
+            // pets table in the database).
+            TextView displayView = (TextView) findViewById(R.id.recommended_view);
+            displayView.setText("Number of rows in Destination database table: " + cursor.getCount());
+        } finally {
+            // Always close the cursor when you're done reading from it. This releases all its
+            // resources and makes it invalid.
+            cursor.close();
+        }
+    }
+
 }
